934703ddab4a6e092033dd6739fc8bc175e2bd3d
Srividya Ravi Sivashankar <srividya.rs.2024@computing.smu.edu.sg>
2025-12-11 09:02:20 +0700

    Implemented hashing for password, so all user passwords in the db are now hashed, ensuring enhanced security.

--- taskmanager-backend/src/main/java/com/eiu/taskmanager/model/User.java
+++ taskmanager-backend/src/main/java/com/eiu/taskmanager/model/User.java
@@ -52,4 +52,9 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+    
+    public String getPassword() {
+    return password;
+    }
+
 }
--- taskmanager-backend/src/main/java/com/eiu/taskmanager/service/UserService.java
+++ taskmanager-backend/src/main/java/com/eiu/taskmanager/service/UserService.java
@@ -4,17 +4,23 @@ import com.eiu.taskmanager.model.User;
 import com.eiu.taskmanager.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Service
 public class UserService {
 
     private final UserRepository repo;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository repo) {
+    public UserService(UserRepository repo, PasswordEncoder passwordEncoder) {
         this.repo = repo;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User createUser(User u) {
+        u.setPassword(passwordEncoder.encode(u.getPassword()));
+        
         // Basic checks (could add email uniqueness check)
         return repo.save(u);
     }
